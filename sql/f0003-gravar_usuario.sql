98c0f534154833076db4677e83eda2991e76c6c2
Jonas Diel <jonasdiel@gmail.com>
2017-08-24 09:33:50 -0300

    Ajustes nas triggers

---
+++ sql/f0003-gravar_usuario.sql
@@ -0,0 +1,44 @@
+﻿CREATE OR REPLACE FUNCTION gravar_usuario() 
+RETURNS TRIGGER AS
+$BODY$
+DECLARE
+v_usuario VARCHAR;
+BEGIN
+v_usuario := NEW.login;
+
+--Verificar para utilizar a senha do usuario (ver como criptografar)
+
+IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_user WHERE usename = v_usuario) THEN
+	EXECUTE 'CREATE USER '||v_usuario||' WITH password ''postgres'' SUPERUSER LOGIN;';
+END IF;
+
+RETURN NEW;
+
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_criar_usuario AFTER INSERT ON usuario FOR EACH ROW EXECUTE PROCEDURE gravar_usuario();
+
+
+--Função para criar primeiro usuário caso ainda não exista
+CREATE OR REPLACE FUNCTION gera_usuario_inicial()
+RETURNS BOOLEAN AS
+$BODY$
+DECLARE
+
+BEGIN
+IF NOT EXISTS (SELECT 1 FROM usuario WHERE login = 'postgres')
+THEN
+	IF NOT EXISTS (SELECT 1 FROM grupo_permissao WHERE descricao = 'Admin') THEN
+		INSERT INTO grupo_permissao(descricao) VALUES ('Admin');
+	END IF;
+	INSERT INTO usuario (nome,senha,login, grupo_permissao_id) VALUES ('Admin', 'senha', 'admin', (select id from grupo_permissao where descricao = 'Admin'));
+END IF;
+
+RETURN TRUE;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+SELECT * FROM gera_usuario_inicial();
