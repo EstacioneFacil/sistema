32a0d9a0aeb4f38f4c143b4953121299b4df6400
Jonas Diel <jonasdiel@gmail.com>
2017-08-24 08:23:36 -0300

    adicionando trigger usuario

---
+++ sql/f0004-gravar_usuario.sql
@@ -0,0 +1,19 @@
+﻿CREATE OR REPLACE FUNCTION gravar_usuario() 
+RETURNS TRIGGER AS
+$BODY$
+DECLARE
+v_usuario VARCHAR;
+v_query TEXT;
+BEGIN
+IF NOT EXISTS (SELECT FROM pg_catalog.pg_user WHERE usename = NEW.login) THEN
+	CREATE ROLE my_user LOGIN PASSWORD 'my_password';
+END IF;
+v_usuario := NEW.login;
+
+v_query := "CREATE ROLE "||v_usuario||" SUPERUSER LOGIN;";
+
+--CREATE ROLE 
+
+END;
+$BODY$
+LANGUAGE plpgsql;
