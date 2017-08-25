7c517b90d2cb78b30a1e8873a557c5e36d850c99
Douglas Hauschild <hauschild.douglas@gmail.com>
2017-08-24 21:03:02 -0300

    Ajustes e adição de colunas nas tabelas do banco

--- sql/estacioneFacil.sql
+++ sql/estacioneFacil.sql
@@ -29,7 +29,9 @@ CREATE TABLE menu (
  descricao VARCHAR(255) NOT NULL,
  ordem INT,
  grupo INT,
- menu_id INT
+ menu INT,
+ menu_pai INT,
+ acao VARCHAR(100) 
 );
 
 ALTER TABLE menu ADD CONSTRAINT PK_menu PRIMARY KEY (id);
@@ -129,32 +131,54 @@ CREATE TABLE movimentacao (
 
 ALTER TABLE movimentacao ADD CONSTRAINT PK_movimentacao PRIMARY KEY (id);
 
-
 ALTER TABLE permissao ADD CONSTRAINT FK_permissao_0 FOREIGN KEY (grupo_permissao_id) REFERENCES grupo_permissao (id);
 ALTER TABLE permissao ADD CONSTRAINT FK_permissao_1 FOREIGN KEY (menu_id) REFERENCES menu (id);
 
-
 ALTER TABLE usuario ADD CONSTRAINT FK_usuario_0 FOREIGN KEY (grupo_permissao_id) REFERENCES grupo_permissao (id);
-
 
 ALTER TABLE vaga ADD CONSTRAINT FK_vaga_0 FOREIGN KEY (area_id) REFERENCES area (id);
 ALTER TABLE vaga ADD CONSTRAINT FK_vaga_1 FOREIGN KEY (tipo_veiculo_id) REFERENCES tipo_veiculo (id);
 
-
 ALTER TABLE auditoria ADD CONSTRAINT FK_auditoria_0 FOREIGN KEY (usuario_id) REFERENCES usuario (id);
 
-
 ALTER TABLE auditoria_detalhe ADD CONSTRAINT FK_auditoria_detalhe_0 FOREIGN KEY (auditoria_id) REFERENCES auditoria (id);
-
 
 ALTER TABLE tabela_preco ADD CONSTRAINT FK_tabela_preco_0 FOREIGN KEY (area_id) REFERENCES area (id);
 ALTER TABLE tabela_preco ADD CONSTRAINT FK_tabela_preco_1 FOREIGN KEY (tipo_veiculo_id) REFERENCES tipo_veiculo (id);
 ALTER TABLE tabela_preco ADD CONSTRAINT FK_tabela_preco_2 FOREIGN KEY (vaga_id) REFERENCES vaga (id);
-
 
 ALTER TABLE movimentacao ADD CONSTRAINT FK_movimentacao_0 FOREIGN KEY (vaga_id) REFERENCES vaga (id);
 ALTER TABLE movimentacao ADD CONSTRAINT FK_movimentacao_1 FOREIGN KEY (anexo_id) REFERENCES anexo (id);
 ALTER TABLE movimentacao ADD CONSTRAINT FK_movimentacao_2 FOREIGN KEY (usuario_id) REFERENCES usuario (id);
 ALTER TABLE movimentacao ADD CONSTRAINT FK_movimentacao_3 FOREIGN KEY (tabela_preco_id) REFERENCES tabela_preco (id);
 
+
+
+-- INSERTS
+INSERT INTO menu (descricao, ordem, grupo, menu) VALUES ('Cadastro', 1, 1, 1);
+INSERT INTO menu (descricao, ordem, grupo, menu) VALUES ('Usuários', 2, 1, 2);
+INSERT INTO menu (descricao, ordem, grupo, menu, menu_pai, acao) VALUES ('Administrar Usuários', 1, 1, 3, 2, 'menuUsuario');
+INSERT INTO menu (descricao, ordem, grupo, menu, menu_pai, acao) VALUES ('Grupos de Permissão', 2, 1, 4, 2, 'menuGrupoPermissao');
+
+
+INSERT INTO grupo_permissao (descricao) VALUES ('Administrador');
+
+
+INSERT INTO usuario (nome, senha, login, grupo_permissao_id) VALUES ('Administrador', 'KM09KLhJyl0=', 'admin', 1);
+
+
+INSERT INTO permissao (grupo_permissao_id, menu_id, visualizar, inserir, editar, excluir) VALUES (1, 3, true, true, true, true);
+INSERT INTO permissao (grupo_permissao_id, menu_id, visualizar, inserir, editar, excluir) VALUES (1, 4, true, true, true, true);
+
+
+-- VIEW
+CREATE VIEW buscar_menu AS
+SELECT m.id, m.descricao, m.ordem, m.grupo, m.menu, m.menu_pai, m.acao FROM menu m WHERE menu_pai IS NULL
+UNION
+SELECT m.id, m.descricao, m.ordem, m.grupo, m.menu, m.menu_pai, m.acao FROM menu m
+INNER JOIN permissao p ON p.menu_id = m.id
+INNER JOIN grupo_permissao gp ON p.grupo_permissao_id = gp.id
+INNER JOIN usuario u ON u.grupo_permissao_id = gp.id
+WHERE p.visualizar = 't' 
+AND u.id = (SELECT id FROM usuario WHERE login = session_user::VARCHAR)
 
