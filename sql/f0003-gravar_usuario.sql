10c3850e47a383a43a63efc9834ebe1770f62a08
Jonas Diel <jonasdiel@gmail.com>
2017-09-08 10:21:59 -0300

    Corrigido problema de não executar scripts iniciais.
    Criado usuário postgres na tablea de usurios

--- sql/f0003-gravar_usuario.sql
+++ sql/f0003-gravar_usuario.sql
@@ -28,13 +28,20 @@ $BODY$
 DECLARE
 
 BEGIN
-IF NOT EXISTS (SELECT 1 FROM usuario WHERE login = 'admin')
-THEN
-	IF NOT EXISTS (SELECT 1 FROM grupo_permissao WHERE descricao = 'Administrador') THEN
+IF NOT EXISTS (SELECT 1 FROM grupo_permissao WHERE descricao = 'Administrador') THEN
 		INSERT INTO grupo_permissao(descricao) VALUES ('Administrador');
 	END IF;
+
+IF NOT EXISTS (SELECT 1 FROM usuario WHERE login = 'admin')
+THEN	
 	INSERT INTO usuario (nome, senha, login, grupo_permissao_id) VALUES ('Administrador', 'KM09KLhJyl0=', 'admin', (select id from grupo_permissao where descricao = 'Administrador'));
 END IF;
+
+IF NOT EXISTS (SELECT 1 FROM usuario WHERE login = 'postgres')
+THEN	
+	INSERT INTO usuario (nome, senha, login, grupo_permissao_id) VALUES ('Postgres', 'KM09KLhJyl0=', 'postgres', (select id from grupo_permissao where descricao = 'Administrador'));
+END IF;
+
 
 RETURN TRUE;
 END;
