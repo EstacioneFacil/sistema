a80d2c8daed60be8b99299f0b036449f98e60848
Douglas <douglas@rsdata.com.br>
2017-12-01 17:28:23 -0200

    Ajustes nas permissoes

---
+++ sql/f0016-ajuste_permissoes.sql
@@ -0,0 +1,15 @@
+﻿DROP VIEW buscar_menu;
+CREATE VIEW buscar_menu AS
+SELECT m.id, m.descricao, m.ordem, m.grupo, m.menu_pai, m.classe, m.visivel FROM menu m WHERE m.menu_pai IS NULL
+UNION
+SELECT m.id, m.descricao, m.ordem, m.grupo, m.menu_pai, m.classe, m.visivel FROM menu m
+INNER JOIN permissao p ON p.menu_id = m.id
+INNER JOIN grupo_permissao gp ON p.grupo_permissao_id = gp.id
+INNER JOIN usuario u ON u.grupo_permissao_id = gp.id
+WHERE p.visualizar = 't'
+AND u.id = (SELECT id FROM usuario WHERE login = session_user::VARCHAR);
+ALTER TABLE menu ADD visivel boolean DEFAULT TRUE;
+INSERT INTO menu (descricao, visivel) VALUES ('Dashboard', false);
+INSERT INTO menu (descricao, visivel) VALUES ('Dashboard - Vagas', false);
+INSERT INTO permissao (grupo_permissao_id, menu_id, visualizar) VALUES (1, 13, true);
+INSERT INTO permissao (grupo_permissao_id, menu_id, visualizar) VALUES (1, 14, true);
